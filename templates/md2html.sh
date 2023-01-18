4618eabd1fdd4647f26f31601a6b2e3566252a0e
i-am-david-liu <davidliu081001@gmail.com>
2023-01-18 12:36:02 -0500

    new post, update template

--- templates/md2html.sh
+++ templates/md2html.sh
@@ -2,6 +2,7 @@
 
 #====================================================================
 # quick and dirty wrapper for Showdown to generate with html headers
+# /!\ PLEASE PLEASE PLEASE IMPROVE THIS IN THE FUTURE
 # 
 # mostly used for blog posts, hence the default stylesheet link
 #
@@ -9,6 +10,8 @@
 
 in=$1
 out=$2
+wordcount=$(wc -w $in | awk '{print $1;}')
+
 cat << EOF > $out
 <!DOCTYPE html>
 <html>
@@ -22,9 +25,17 @@ cat << EOF > $out
     <title>dliu.xyz</title>
   </head>
   <body>
+    <hr>
+    <p>
+      <span class="left mono"></span>
+      <span class="right mono"$wordcount words</span>
+    </p>
+    <div style="clear: both;"></div>
 EOF
 showdown makehtml -a -i $in -o $out
 cat << EOF >> $out
+ 
+    </br>
     <hr>
     <p class="center">
       <a href="/posts">back</a>
